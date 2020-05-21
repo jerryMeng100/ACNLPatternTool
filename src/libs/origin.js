178b07cbf0eea4460ed2cef02c07a1454449c41c
clkelley <clkelley@stanford.edu>
2020-05-20 20:43:41 -0700

    full circle done
    create, upload, search, view, and recent all working. tags blocked, but we're going to make our own tagging system (it will need to be added back into search post-hoc)

--- src/libs/origin.js
+++ src/libs/origin.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const { API_URL } = process.env;
 import firebase from "firebase";
 import md5 from "md5"
+import ACNLFormat from './ACNLFormat';
+import lzString from 'lz-string';
 
 //Firebase project configuration
 const firebaseConfig = {
@@ -42,136 +44,204 @@ const encodeQueryParams = (params) => {
 };
 
 //Upload
-const upload = async (pattData, styleA, styleB, styleC, typeA, typeB, typeC, NSFW) => {
-  const response = await api.post('api.php', {
-    pattern:pattData,
-    styletag_a:styleA,
-    styletag_b:styleB,
-    styletag_c:styleC,
-    typetag_a:typeA,
-    typetag_b:typeB,
-    typetag_c:typeC,
-    nokids:(NSFW?"Y":"")
-  });
-  return response.data;
-};
-
-//NEW VERSION OF UPLOAD: UNFINISHED
 // const upload = async (pattData, styleA, styleB, styleC, typeA, typeB, typeC, NSFW) => {
-//   const hash = md5(pattData)
-//   return db.collection("patterns").doc(hash).set(
-//     {
-//         pattern:pattData,
-//         styletag_a:styleA,
-//         styletag_b:styleB,
-//         styletag_c:styleC,
-//         typetag_a:typeA,
-//         typetag_b:typeB,
-//         typetag_c:typeC,
-//         nokids:(NSFW?"Y":"")
-//     }
-//   ).then(function() {
-//       console.log("Document successfully written!");
-//       return {"upload":hash}
-//   })
-//   .catch(function(error) {
-//       console.error("Error writing document: ", error);
-//       return {"error":"cannot write"}
+//   const response = await api.post('api.php', {
+//     pattern:pattData,
+//     styletag_a:styleA,
+//     styletag_b:styleB,
+//     styletag_c:styleC,
+//     typetag_a:typeA,
+//     typetag_b:typeB,
+//     typetag_c:typeC,
+//     nokids:(NSFW?"Y":"")
 //   });
+//   return response.data;
+// };
+
+// const create_pattern = (pattData, styleA, styleB, styleC, typeA, typeB, typeC, NSFW) => {
+//   //the thing we return to load (what is fetched in bytes by view) is a base64 version of ACNL.
+//   //load takes a string with "B6|" in front and calls atob on it.
+//   //upload is handed a ACNL with btoa called on it.
+//   //we might just simply need to append the type.
+//   var newPatternObject = ACNLFormat(pattData);
 // }
+//NEW VERSION OF UPLOAD: UNFINISHED
+const upload = async (pattData, styleA, styleB, styleC, typeA, typeB, typeC, NSFW) => {
+  const hash = md5(pattData)
+  const pattern_string = "B6|" + pattData
+  const url = "/editor#H:" + hash
+  const patternObj = new ACNLFormat(atob(pattData));
+  const title = patternObj.title;
+  const author = patternObj.creator[0];
+  const town = patternObj.town[0];
+  const upload_date = Date.now()
+  //bytes
+  //title
+  //url
+  //author
+  //town
+  return db.collection("patterns").doc(hash).set(
+    {
+        bytes:pattern_string,
+        url:url,
+        title:title,
+        author:author,
+        town:town,
+        upload_date:firebase.firestore.Timestamp.fromDate(new Date()),
+        styletag_a:styleA,
+        styletag_b:styleB,
+        styletag_c:styleC,
+        typetag_a:typeA,
+        typetag_b:typeB,
+        typetag_c:typeC,
+        nokids:(NSFW?"Y":"")
+    }
+  ).then(function() {
+      console.log("Document successfully written!");
+      return {"upload":hash}
+  })
+  .catch(function(error) {
+      console.error("Error writing document: ", error);
+      return {"error":"cannot write"}
+  });
+}
 
 
 // Search
-const search = async (q, options) => {
-  const { nsfc, unapproved: letsgetdangerous } = options;
-  const params = encodeQueryParams({
-    q,
-    nsfc: Number(nsfc),
-    letsgetdangerous: Number(letsgetdangerous),
-  });
-  const response = await api.get(`api.php${params}`);
-  return response.data;
-};
+// const search = async (q, options) => {
+//   const { nsfc, unapproved: letsgetdangerous } = options;
+//   const params = encodeQueryParams({
+//     q,
+//     nsfc: Number(nsfc),
+//     letsgetdangerous: Number(letsgetdangerous),
+//   });
+//   const response = await api.get(`api.php${params}`);
+//   return response.data;
+// };
 
 // // Open single pattern
-const view = async (hash) => {
-  const response = await api.get(`api.php${encodeQueryParams({view: hash})}`);
-  console.log("Document data:", response.data);
-  return response.data;
-};
+// const view = async (hash) => {
+//   const response = await api.get(`api.php${encodeQueryParams({view: hash})}`);
+//   console.log("Document data:", response.data);
+//   return response.data;
+// };
+const search = async (query, options) => {
+  return db.collection("patterns").get().then(function(querySnapshot) {
+    var all_results = []
+    querySnapshot.forEach(function(doc) {
+        const data = doc.data()
+        //check title, author, or town.
+        if(data.town.toLowerCase().includes(query.toLowerCase()) ||
+           data.title.toLowerCase().includes(query.toLowerCase()) ||
+           data.author.toLowerCase().includes(query.toLowerCase())){
+          all_results.push(doc.data())
+        }
+        //in future, check tags as well!
+
+        // doc.data() is never undefined for query doc snapshots
+        //data must contain:
+          //bytes
+          //title
+          //url
+          //author
+          //town
+        //console.log(doc.id, " => ", doc.data());
+    });
+    return all_results;
+  });
+}
+
 
 //8549fe81ea6fb5acbea2f7530c4a14a9
 
 //NEW VERSION OF VIEW
-// const view = async (hash) => {
-//   return db.collection("patterns").doc(hash).get().then(function(doc) {
-//     if (doc.exists) {
-//         console.log("Document data:", doc.data().bytes);
-//         return doc.data().bytes;
-//     } else {
-//         // doc.data() will be undefined in this case
-//         console.log("No such document!");
-//         return ""
-//     }
-// }).catch(function(error) {
-//     console.log("Error getting document:", error);
-//     return ""
-// });
-//
-// }
+const view = async (hash) => {
+  return db.collection("patterns").doc(hash).get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data().bytes);
+        return doc.data().bytes;
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        return ""
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+    return ""
+});
+
+}
 
 // Recent uploads
+// const recent = async (options) => {
+//   const { nsfc, unapproved: letsgetdangerous } = options;
+//   const params = encodeQueryParams({
+//     recent: 1,
+//     nsfc: Number(nsfc),
+//     letsgetdangerous: Number(letsgetdangerous),
+//   });
+//   const response = await api.get(`api.php${params}`);
+//   return response.data;
+// };
+
 const recent = async (options) => {
-  const { nsfc, unapproved: letsgetdangerous } = options;
-  const params = encodeQueryParams({
-    recent: 1,
-    nsfc: Number(nsfc),
-    letsgetdangerous: Number(letsgetdangerous),
-  });
-  const response = await api.get(`api.php${params}`);
-  return response.data;
-};
-
-const modLogIn = async (username, password) => {
-  try {
-    const response = await api.post("api.php", {
-      user: username,
-      pass: password
+  return db.collection("patterns").orderBy("upload_date", "desc").get().then(function(querySnapshot) {
+    var all_results = []
+    querySnapshot.forEach(function(doc) {
+        all_results.push(doc.data())
+        // doc.data() is never undefined for query doc snapshots
+        //data must contain:
+          //bytes
+          //title
+          //url
+          //author
+          //town
+        //console.log(doc.id, " => ", doc.data());
     });
-    if (response.data.error){
-      throw new Error(response.data.error);
-      return "";
-    }
-    return response.data.token;
-  }
-  catch (error) {
-    if (error.response.status !== 401) throw error;
-    return "";
-  }
-};
-
-const modPending = async (token) => {
-  const response = await api.get(`api.php${encodeQueryParams({modqueue: 1, token})}`);
-  return response.data;
-};
-
-const modApprove = async (hash, options, token) => {
-  const response = await api.post("api.php", {
-    ...options,
-    approve: hash,
-    token,
+    return all_results;
   });
-  return response.data;
-};
+}
+
+// const modLogIn = async (username, password) => {
+//   try {
+//     const response = await api.post("api.php", {
+//       user: username,
+//       pass: password
+//     });
+//     if (response.data.error){
+//       throw new Error(response.data.error);
+//       return "";
+//     }
+//     return response.data.token;
+//   }
+//   catch (error) {
+//     if (error.response.status !== 401) throw error;
+//     return "";
+//   }
+// };
+
+// const modPending = async (token) => {
+//   const response = await api.get(`api.php${encodeQueryParams({modqueue: 1, token})}`);
+//   return response.data;
+// };
+
+// const modApprove = async (hash, options, token) => {
+//   const response = await api.post("api.php", {
+//     ...options,
+//     approve: hash,
+//     token,
+//   });
+//   return response.data;
+// };
 
 // exporting as delete, delete is a keyword :(
-const modDelete = async (hash, token) => {
-  const response = await api.post("api.php", {
-    wipepattern: hash,
-    token: token
-  });
-  return response.data;
-};
+// const modDelete = async (hash, token) => {
+//   const response = await api.post("api.php", {
+//     wipepattern: hash,
+//     token: token
+//   });
+//   return response.data;
+// };
 
 
 const tags_style = [
@@ -218,10 +288,10 @@ export default {
   search,
   recent,
   view,
-  modLogIn,
-  modPending,
-  modApprove,
-  modDelete,
+  // modLogIn,
+  // modPending,
+  // modApprove,
+  // modDelete,
   tags_style,
   tags_type
 };
